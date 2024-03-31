aaa0f6b087a490487f61a77536b74587feb657cc
vivek <viveksanthosh.in@gmail.com>
2024-03-31 20:04:06 +0530

    added google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes, useRoutes } from "react-router-dom";
 import Home from "./Home";
+import Login from "./Login";
 import Other from "./Other";
 import AboutLayout from "./AboutLayout";
 import "./App.css";
 import { Suspense, lazy } from "react";
 import delay from "delay";
+
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
 
 const About = lazy(() => {
   return delay(1000).then(() => import("./About"));
@@ -17,22 +20,25 @@ function App() {
       element: <Other />,
     },
   ]);
+
   return (
     <>
-      <Routes>
-        <Route path="/about" element={<p>Parent</p>}></Route>
-      </Routes>
-      <p>App</p>
-      {element}
-      <Suspense fallback={<Loader />}>
+      <GoogleOAuthProvider clientId="177736600735-rg6gn26be2p7950igvforgqhmf0nntuh.apps.googleusercontent.com">
         <Routes>
-          <Route path="/" Component={Home} />
-          <Route Component={AboutLayout} path="/about">
-            <Route index Component={About} />
-            <Route path="other" Component={Other} />
-          </Route>
+          <Route path="/login" Component={Login}></Route>
         </Routes>
-      </Suspense>
+        <p>App</p>
+        {element}
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route Component={AboutLayout} path="/about">
+              <Route index Component={About} />
+              <Route path="other" Component={Other} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </GoogleOAuthProvider>
     </>
   );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,18 @@
 // generate placeholder jsx
 
+import { useGoogleLogin } from "@react-oauth/google";
 import { Link } from "react-router-dom";
 
 export default function About() {
+  const auth = useGoogleLogin({
+    onSuccess: (data) => {
+      console.log({ data });
+    },
+    onError: (data) => {
+      console.log({ data });
+    },
+  });
+
   return (
     <h1>
       <Link to="/about">About</Link>
